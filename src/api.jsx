66505aa511b312b924ece07f948d7b8ab06a6552
Jessica Jiao <j7jiao@uwaterloo.ca>
2024-05-19 18:53:18 -0400

    Initial commit

---
+++ src/api.jsx
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'db4d0b9516msh05e86254b18ccd2p1ccbabjsn80599721f040',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo';
