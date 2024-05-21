2f832834a768a39caf5511ff206e7c0be234df83
Jessica Jiao <j7jiao@uwaterloo.ca>
2024-05-20 21:20:12 -0400

    implemented search bar and added current-weather component

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
-import './search.css'
-import { GEO_API_URL, geoApiOptions } from '../../api';
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import "./search.css";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
-
   const [search, setSearch] = useState(null);
 
   const loadOptions = (inputValue) => {
-    return fetch(`${GEO_API_URL}/cities`, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-  }
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
 
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
-  }
+  };
 
   return (
-    <AsyncPaginate className='container'
-      placeholder='Search for city'
+    <AsyncPaginate
+      className="container"
+      placeholder="Search for city"
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
       loadOptions={loadOptions}
     />
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
